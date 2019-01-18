e3bac04fd8ea6ce26dd0ac2ff59053f18e314cb1
Lata <lata@newgenapps.com>
2019-01-18 18:28:45 +0530

    Initial commit

---
+++ Translate/ViewController.swift
@@ -0,0 +1,28 @@
+//  
+//  ViewController.swift
+//  Translate
+//  Using Swift 4.0
+//
+//  Created by Abhishek Singh on 18/01/19.
+//  Copyright © 2019 NGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var selectSourceButton: UIButton!
+    @IBOutlet weak var sourceTextView: UITextView!
+    @IBOutlet weak var targetSourceButton: UIButton!
+    @IBOutlet weak var targetTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
